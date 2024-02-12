eb1b4717e4b78717909bc43891324030f5a82711
Myrhorodska <schatznata87@gmail.com>
2024-02-12 11:12:59 +0100

    all tests

--- src/main/java/pages/CommonActionWithElements.java
+++ src/main/java/pages/CommonActionWithElements.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.time.Duration;
 
 
